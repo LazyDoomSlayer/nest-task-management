596584d6c6c9769cee91e7ba49b690dfac139e5a
LazyDoomSlayer <lazydoomslayer@gmail.com>
2025-02-08 13:48:37 +0100

    feat: add password validation

---
+++ src/assets/patterns.ts
@@ -0,0 +1,2 @@
+export const DEFAULT_PASSWORD_PATTERN =
+  /((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/;
--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,4 +1,15 @@
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
+import { DEFAULT_PASSWORD_PATTERN } from 'src/assets/patterns';
+
 export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(16)
   username: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(64)
+  @Matches(DEFAULT_PASSWORD_PATTERN, { message: 'Your password is shithole.' })
   password: string;
 }
