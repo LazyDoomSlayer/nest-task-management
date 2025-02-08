eca42c52d49eb40a554e815b17e042fbd391da6f
LazyDoomSlayer <lazydoomslayer@gmail.com>
2025-02-08 14:26:42 +0100

    feat: add sign in

--- src/auth/auth.enum.ts
+++ src/auth/auth.enum.ts
@@ -1,3 +1,4 @@
 export enum EAuthErrorMessages {
   USER_EXISTS = 'Username already exists.',
+  WRONG_CREDENTIALS = 'Please check your login credentials.',
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersRepository } from './user.repository';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+
+import * as bcyrpt from 'bcrypt';
+import { EAuthErrorMessages } from './auth.enum';
 
 @Injectable()
 export class AuthService {
@@ -8,5 +11,16 @@ export class AuthService {
 
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return this.usersRepository.createUser(authCredentialsDto);
+  }
+
+  async signIn(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+    const { username, password } = authCredentialsDto;
+
+    const user = await this.usersRepository.findOne({ where: { username } });
+    if (user && (await bcyrpt.compare(password, user.password))) {
+      return 'success';
+    }
+
+    throw new UnauthorizedException(EAuthErrorMessages.WRONG_CREDENTIALS);
   }
 }
